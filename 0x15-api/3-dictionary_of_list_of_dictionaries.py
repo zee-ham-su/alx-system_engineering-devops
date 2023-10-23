a60b760ba4aa573bee3b7d612a50140181f8103d
zee-ham-su <hsufiian@yahoo.com>
2023-10-23 16:42:29 +0000

    json

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,22 +5,36 @@ import requests
 import sys
 
 
+import json
+import requests
+import sys
+
+
 if __name__ == '__main__':
     base_url = "https://jsonplaceholder.typicode.com/users"
-    employees = {}
 
-    for employee_id in range(1, 11):  # Assuming employee IDs are from 1 to 10
-        url = f"{base_url}/{employee_id}"
-        response = requests.get(url)
-        employee_data = response.json()
-        employee_name = employee_data.get('name')
+    response = requests.get(base_url)
+    users = response.json()
 
-        todo_url = f"{url}/todos"
-        tasks = requests.get(todo_url).json()
+    employee_data = {}
+    for user in users:
+        user_id = user.get('id')
+        username = user.get('username')
+        user_url = (
+            'https://jsonplaceholder.typicode.com/users/{}'
+            .format(user_id)
+        )
+        todo_url = user_url + '/todos/'
+        response = requests.get(todo_url)
+        tasks = response.json()
 
-        tasks_data = [{"username": employee_name, "task": task['title'],
-                       "completed": task['completed']} for task in tasks]
-        employees[str(employee_id)] = tasks_data
-
+        employee_data[user_id] = [
+                {
+                    "task": task.get('title'),
+                    "completed": task.get('completed'),
+                    "username": username
+                }
+                for task in tasks
+            ]
     with open('todo_all_employees.json', 'w') as json_file:
-        json.dump(employees, json_file)
+        json.dump(employee_data, json_file)
