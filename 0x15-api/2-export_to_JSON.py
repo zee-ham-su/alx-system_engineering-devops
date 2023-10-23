a369e5e3fd01249b43537f6191617ee53c08526b
zee-ham-su <hsufiian@yahoo.com>
2023-10-23 16:31:34 +0000

    json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,22 +9,25 @@ import sys
 if __name__ == '__main__':
     employee_id = sys.argv[1]
     base_url = "https://jsonplaceholder.typicode.com/users"
-    url = f"{base_url}/{employee_id}"
+    url = base_url + "/" + employee_id
 
     response = requests.get(url)
-    employee_data = response.json()
-    employee_name = employee_data.get('name')
+    username = response.json().get('username')
 
-    todo_url = f"{url}/todos"
-    tasks = requests.get(todo_url).json()
+    todo_url = url + "/todos"
+    response = requests.get(todo_url)
+    tasks = response.json()
 
-    user_tasks = [{"task": task['title'], "completed": task['completed'],
-                   "username": employee_name} for task in tasks]
+    employee_data = {
+        employee_id: [
+            {
+                "task": task.get('title'),
+                "completed": task.get('completed'),
+                "username": username
+            }
+            for task in tasks
+        ]
+    }
 
-    output_data = {employee_id: user_tasks}
-    output_file = f"{employee_id}.json"
-
-    with open(output_file, 'w') as json_file:
-        json.dump(output_data, json_file)
-
-    print(f"Data exported to {output_file}")
+    with open('{}.json'.format(employee_id), 'w') as json_file:
+        json.dump(employee_data, json_file)
