3889192bf396a7067e828fa7b8dbcdef7792afed
zee-ham-su <hsufiian@yahoo.com>
2023-11-07 05:30:50 -0800

    count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,52 +1,46 @@
 #!/usr/bin/python3
-"""
-function that queries the Reddit API
-"""
+"""function that queries the Reddit API"""
 
 import requests
 
 
-def count_words(subreddit, word_list, after=None, counts=None):
-    """
-    Recursive function that queries the Reddit API, parses the titles
-    of all hot articles, and prints a sorted count of given keywords.
-    """
-    if counts is None:
-        counts = {}
+def count_words(subreddit, word_list, after="", word_counts=None):
+    """Count occurrences of words in subreddit titles"""
 
-    user_agent = {
-        'User-agent': (
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-            '(KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.3'
-        )
-    }
+    if after == "":
+        word_counts = {word.lower(): 0 for word in word_list}
 
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(subreddit)
-    if after:
-        url += '&after={}'.format(after)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {'after': after}
+    headers = {'user-agent': 'bhalut'}
 
-    try:
-        response = requests.get(url, headers=user_agent)
-        response.raise_for_status()
+    response = requests.get(url, params=params,
+                            allow_redirects=False, headers=headers)
+
+    if response.status_code == 200:
         data = response.json()
-        posts = data.get('data', {}).get('children', [])
 
-        for post in posts:
-            title = post['data']['title'].lower()
+        for post in data['data']['children']:
+            for word in post['data']['title'].split():
+                word_lower = word.lower()
+                if word_lower in word_counts:
+                    word_counts[word_lower] += 1
 
-            for word in word_list:
-                if title.count(word.lower()):
-                    counts[word.lower()] = counts.get(word.lower(), 0) + \
-                       title.count(word.lower())
+        next_after = data['data']['after']
 
-        next_after = data.get('data', {}).get('after')
-        if next_after:
-            count_words(subreddit, word_list, next_after, counts)
+        if next_after is None:
+            save = set()
+            for i, word1 in enumerate(word_list):
+                for j, word2 in enumerate(word_list[i+1:], start=i+1):
+                    if word1.lower() == word2.lower():
+                        save.add(j)
+                        word_counts[word1.lower()] += word_counts[word2.lower()]
+
+            sorted_word_counts = sorted(word_counts.items(),
+                                        key=lambda x: (-x[1], x[0]))
+
+            for word, count in sorted_word_counts:
+                if count > 0 and word not in save:
+                    print("{}: {}".format(word, count))
         else:
-            sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-
-            for word, count in sorted_counts:
-                print('{}: {}'.format(word, count))
-
-    except requests.exceptions.RequestException as req_exc:
-        print('Error: {}'.format(req_exc))
+            count_words(subreddit, word_list, next_after, word_counts)
