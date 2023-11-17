fe62ab2c9870d3fa5b7979eb9a8dd2fb943665cc
zee-ham-su <hsufiian@yahoo.com>
2023-11-17 19:05:41 +0000

    puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -6,7 +6,7 @@
 exec { 'fix-for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/',
-  } ->
+  }
 
 # Restart Nginx after open files limit update
 
